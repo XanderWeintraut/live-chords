f4e70384d418d1de419e18ca3fa69b556d4b46a1
Xander Weintraut <xweintraut@gmail.comglobal user.email xweintraut@gmail.com>
2019-09-14 18:35:16 -0400

    I think this should work once merged with UI stuff

--- app/src/main/java/com/example/live_chords/SecondActivity.java
+++ app/src/main/java/com/example/live_chords/SecondActivity.java
@@ -1,6 +1,55 @@
 package com.example.live_chords;
 
+/*
+ *
+ * When button is pressed, begins recording audio and processes it.
+ *
+ * Based heavily off of example found here: https://developer.android.com/guide/topics/media/mediarecorder#java
+ *
+ * @Author Xander Weintraut
+ * @Version 6:02 PM, September 14 2019
+ *
+ */
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import android.core.app.ActivityCompat;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ToggleButton;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+
+    private ToggleButton button = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityCompat.requestPermissions(this,)
+        setContentView(R.layout.activity_second);
+    }
+
+
 }
