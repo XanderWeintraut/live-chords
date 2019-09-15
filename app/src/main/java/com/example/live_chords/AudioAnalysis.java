80b2f8d2fdaa8e582d2ca7f7477d8dbb1f02dffd
XanderWeintraut <xweintraut@gmail.com>
2019-09-15 04:03:25 -0400

    Edited Audio Analysis file

--- app/src/main/java/com/example/live_chords/AudioAnalysis.java
+++ app/src/main/java/com/example/live_chords/AudioAnalysis.java
@@ -1,6 +1,27 @@
 package com.example.live_chords;
 
+import
+
 import androidx.lifecycle.ViewModel;
 
 public class AudioAnalysis extends ViewModel {
+
+    AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+
+    PitchDetectionHandler pdh = new PitchDetectionHandler() {
+        @Override
+        public void handlePitch(PitchDetectionResult result,AudioEvent e) {
+            final float pitchInHz = result.getPitch();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    TextView text = (TextView) findViewById(R.id.textView1);
+                    text.setText("" + pitchInHz);
+                }
+            });
+        }
+    };
+    AudioProcessor p = new PitchProcessor(PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+dispatcher.addAudioProcessor(p);
+new Thread(dispatcher,"Audio Dispatcher").start();
 }
